14e77248ca8a8fb3b529a944e3586841ad97a0ae
ISale7 <ibrahim@zenhr.com>
2021-11-08 18:17:49 +0200

    adding a scaffold form for the books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_131912) do
+ActiveRecord::Schema.define(version: 2021_11_08_124207) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.text "description"
+    t.integer "price"
+    t.boolean "availability", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
